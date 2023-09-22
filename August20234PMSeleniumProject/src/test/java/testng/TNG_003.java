556e0a5c56cd73eaccfe3f152a9d7af7d5764600
ravilella1234 <masters.selenium@gmail.com>
2023-09-22 16:29:32 +0530

    TestNG Day-2

---
+++ August20234PMSeleniumProject/src/test/java/testng/TNG_003.java
@@ -0,0 +1,52 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import launchings.BaseTest;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class TNG_003 extends BaseTest
+{
+	
+  @BeforeMethod
+  public void startProcess() throws Exception 
+  {
+	  System.out.println("BeforeMethod");
+	    init();
+		test = report.createTest("TNG_003");
+		test.log(Status.INFO, "init all the properties files...");		
+		
+		launch("chromebrowser");
+		test.log(Status.PASS, "Opening browser : " + p.getProperty("chromebrowser"));
+		
+		navigateUrl("amazonurl");
+		test.log(Status.PASS, "Navigating to url : " + childprop.getProperty("amazonurl"));
+  }
+  
+  @Test
+  public void amazon() 
+  {
+	  System.out.println("F method");
+	    selectOption("amazondropbox_id","Books");
+		test.log(Status.PASS, "Selected the option Books By using the locator : " + orprop.getProperty("amazondropbox_id"));
+		
+		typeText("amazonserachtext_id","Harry Potter");
+		test.log(Status.PASS, "Selected the option Books By using the locator : " + orprop.getProperty("amazondropbox_id"));
+		
+		clickElement("amazonsearchbutton_xpath");
+		test.log(Status.FAIL, "Clicked on Element By USing locator : " + orprop.getProperty("amazonsearchbutton_xpath"));
+  }
+
+  @AfterMethod
+  public void endProcess() 
+  {
+	  System.out.println("After Method");
+	  driver.quit();
+	  report.flush();
+  }
+
+}
